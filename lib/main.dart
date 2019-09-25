ee4876e6601f69d867070db6aa654c9781faf466
Kevin <Aok4001032@gmail.com>
2019-09-24 20:44:01 -0400

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker_modern/image_picker_modern.dart';
 import 'package:fit_image/fit_image.dart';
+import 'package:ml_kit/main.dart';
 
 void main() => runApp(MyApp());
 
@@ -37,6 +39,20 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  //Metodo que llamara al servicio
+  Future readText() async {
+    FirebaseVisionImage ourImage = FirebaseVisionImage.fromFile(_image);
+    TextRecognizer recognizedText = FirebaseVision.instance.textRecognizer();
+    VisionText readText = await recognizedText.processImage(ourImage);
+    //Metodo que manejara la respuesta
+    for (TextBlock bloque in readText.blocks) {
+      for (TextLine linea in bloque.lines) {
+        print(linea.text);
+      }
+    }
+  }
+
+//Metodo que manejara el codigo de barras
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,23 +63,30 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-             _image == null ? Text('No image selected.') : 
-            new SizedBox(
-                        width: 200,
-              height: 350,
-              child: new FitImage(
-                child: Image.file(_image),
-                fit: BoxFit.fitWidth,
-              ),
+            _image == null
+                ? Text('No image selected.')
+                : new SizedBox(
+                    width: 200,
+                    height: 350,
+                    child: new FitImage(
+                      child: Image.file(_image),
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ),
+            FloatingActionButton(
+              onPressed: getImage,
+              tooltip: 'Escoge una imagen',
+              child: Icon(Icons.add_photo_alternate),
             )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
+        onPressed: readText,
         tooltip: 'Escoge una imagen',
-        child: Icon(Icons.add_photo_alternate),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.adjust),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
